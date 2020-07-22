0f924d56cad793862efa03a399843dc881d2c13e
qiyun <1809275454@qq.com>
2020-07-22 15:29:29 +0800

    初始化

---
+++ templates/compile/%%1E^1E4^1E44B294%%footers.tpl.php
@@ -0,0 +1,12 @@
+<?php /* Smarty version 2.6.25, created on 2020-07-22 10:30:23
+         compiled from ./ffsm/footers.tpl */ ?>
+<footer class="footer_severs">
+  <div class="ocrloader">
+    <em></em>
+    <img src="/weixin.png" alt="二维码">
+    <span></span>
+  </div>
+  <p>长按二维码加客服微信</p>
+  <p>在线服务时间: 周一至周五9:00-18:00</p>
+</footer>
+</footer>
